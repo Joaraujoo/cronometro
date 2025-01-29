d9cef5e5c91eb9b2f800be69e5d93b592ab5b8f1
Joao Araujo <163578593+Joaraujoo@users.noreply.github.com>
2025-01-29 15:18:51 -0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -2,31 +2,33 @@
 var i = 0
 var interval
 
-function cronometro(){
-
-    interval = setInterval(function(){
-
+function cronometro() {
+    interval = setInterval(function () {
         i += 1
 
-        const segundos = (i/100).toFixed(2)
-        document.getElementById('cronometro').innerHTML = segundos
+        const segundosTotais = (i / 100).toFixed(2)
+        const minutos = Math.floor(segundosTotais / 60)
+        const segundos = Math.floor(segundosTotais % 60)
 
-        const minutos = segundos * 1
+        
+        const minutosFormatados = minutos.toString().padStart(2, '0')
+        const segundosFormatados = segundos.toString().padStart(2, '0')
 
-        if(minutos === 60 ){
+        document.getElementById('cronometro').innerHTML = `${minutosFormatados}:${segundosFormatados}`
+
+        if (minutos === 60) {
             i = 0
             clearInterval(interval)
         }
-        
-    },10)
 
+    }, 10)
 }
 
-function parar(){
+function parar() {
     clearInterval(interval)
 }
 
-function redefinir(){
-    document.getElementById('cronometro').innerHTML = '0:00'
+function redefinir() {
+    document.getElementById('cronometro').innerHTML = '00:00'
     i = 0
 }
